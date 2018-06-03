80283ded4eedc84a0def06b0ca1a79a6cc01c5cd
isel42225 <42225@alunos.isel.ipl.pt>
2018-06-03 11:49:05 +0100

    Process request

--- TP3/AsyncFileCopy/AsyncFileCopy/AsyncFileCopy.h
+++ TP3/AsyncFileCopy/AsyncFileCopy/AsyncFileCopy.h
@@ -1,8 +1,20 @@
 #pragma once 
 #include <Windows.h>
 
+#define BUFFER_SIZE 4096
 
 typedef VOID(*AsyncCallback) (LPVOID userCtx, DWORD status ,UINT64 transferedBytes);
+
+typedef struct OperCtx {
+	OVERLAPPED ovr;
+	HANDLE fIn;
+	HANDLE fOut;
+	BYTE buffer[BUFFER_SIZE];
+	UINT64 currPos;
+	AsyncCallback cb;
+	DWORD r_w;	//signal if is read or write
+	LPVOID userCtx;
+} OPER_CTX, *POPER_CTX;
 
 
 BOOL AsyncInit();
@@ -10,3 +22,4 @@ BOOL AsyncInit();
 BOOL CopyFileAsync(PCSTR srcFile, PCSTR dstFile, AsyncCallback cb, LPVOID userCtx);
 
 VOID AsyncTerminate();
+
